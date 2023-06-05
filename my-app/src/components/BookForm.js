341b9bf5b65d1606186a392b63bc36f8378eaf0e
Katherine West <katherinewest98@yahoo.com>
2023-06-05 16:50:58 -0500

    Completed books page

---
+++ my-app/src/components/BookForm.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { useState } from "react";
+
+function BookForm({ array, setArray }) {
+  const initialFormValues = {
+    name: "",
+    description: "",
+    image: "",
+    purchaseUrl: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newBook = {
+      name: formData.name,
+      description: formData.description,
+      image: formData.image,
+      purchaseUrl: formData.purchaseUrl,
+      likes: 0,
+    };
+
+    const resourceExists = array.some((item) => item.link === formData.link);
+
+    if (resourceExists) {
+      alert("This resource already exists!");
+    } else {
+      fetch("http://localhost:3000/books", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+
+        body: JSON.stringify(newBook),
+      })
+        .then((resp) => resp.json())
+        .then((newBook) => setArray([newBook, ...array]));
+    }
+
+    setFormData(initialFormValues);
+  };
+
+  return (
+    <div className="form-section">
+      <div className="form container m-auto px-2 mt-10 mb-10" id="contribute">
+        <h2>Add a Book</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Title"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="image"
+            placeholder="Image URL"
+            value={formData.image}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="link"
+            placeholder="Purchase Link"
+            value={formData.purchaseUrl}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default BookForm;
--- my-app/src/components/BookItem.js
+++ my-app/src/components/BookItem.js
@@ -1,36 +1,91 @@
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-function BookItem({ image, title, description, purchaseUrl }) {
+function BookItem({ 
+    image, 
+    title, 
+    description, 
+    purchaseUrl,
+    likes,
+    id,
+    book,
+    booksArray,
+    setBooksArray
+ }) {
 
     const [showFullDescription, setShowFullDescription] = useState(false)
     function handleDescClick() {
         setShowFullDescription((showFullDescription) => !showFullDescription)
     }
 
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleClick = () => {
+      const updatedLikes = isLiked ? likes - 1 : likes + 1;
+      setIsLiked(!isLiked);
+  
+      const updateObj = {
+        likes: updatedLikes,
+      };
+      fetch(`http://localhost:3000/books/${id}`, {
+        method: "PATCH",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updateObj),
+      })
+        .then((resp) => resp.json())
+        .then((updatedBook) => {
+          const newBooksArray = booksArray.map((book) =>
+            book.id === id ? updatedBook : book
+          );
+          setBooksArray(newBooksArray);
+        });
+    };
+
     return (
         <div>
-            <Card sx={{ maxWidth: 370, minHeight: 400, marginBottom: "20px" }}>
-                <CardMedia sx={{ height: 250 }} image={image} />
-                <CardContent sx={{ minHeight: 150 }}>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography onClick={handleDescClick} style={{cursor:'pointer'}} variant="body2" color="text.secondary">
-                        {showFullDescription ? description : description.substring(0, 100) + "..."}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button href={purchaseUrl} target="_blank" size="small">Buy Now</Button>
-                </CardActions>
-            </Card>
+          <Card sx={{ maxWidth: 370, minHeight: 410, marginBottom: "20px" }}>
+            <CardMedia sx={{ height: 250 }} image={image} />
+            <div className="card-content">
+              <div className="likes">
+                <a className="like-button" onClick={handleClick}>
+                  {isLiked ? "♥" : "♡"}
+                </a>
+                Likes: {likes}
+              </div>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                lineHeight="1.2em"
+                fontSize="15pt"
+              >
+                {title}
+              </Typography>
+              <Typography
+                onClick={handleDescClick}
+                style={{ cursor: "pointer" }}
+                variant="body2"
+                color="text.secondary"
+                sx={{ minHeight: 10 }}
+              >
+                {showFullDescription
+                  ? description
+                  : description.substring(0, 120) + "..."}
+              </Typography>
+            </div>
+            <div className="card-action">
+              <a href={purchaseUrl} target="_blank">
+                <button className="learn-more">Buy Now</button>
+              </a>
+            </div>
+          </Card>
         </div>
-    );
+      );
 
 }
 
