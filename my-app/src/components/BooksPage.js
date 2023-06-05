94efe5445c3df941b1b527c77313c01576fe06b7
Mai <garcia.ton94@gmail.com>
2023-06-05 13:19:27 -0400

    grammar form added

--- my-app/src/components/BooksPage.js
+++ my-app/src/components/BooksPage.js
@@ -20,7 +20,7 @@ function BooksPage() {
         subTitle="Read Some Books"
         background="https://a.cdn-hotels.com/gdcs/production81/d60/e414d9a4-df1b-4e19-976f-b83e8a1b2c8d.jpg?impolicy=fcrop&w=1600&h=1066&q=medium"
       />
-      <div className="container m-auto px-2 mt-10 mb-10">
+      <div className="container m-auto px-2 mt-20 mb-20">
         <div className="grid grid-cols-3 gap-3">
           {booksArray.map((book) => (
             <BookItem
---
+++ my-app/src/components/GrammarForm.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState } from "react";
+
+function GrammarForm({ array, setArray }) {
+  const initialFormValues = {
+    name: "",
+    description: "",
+    image: "",
+    link: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newGrammar = {
+      name: formData.name,
+      description: formData.description,
+      image: formData.image,
+      link: formData.link,
+      likes: 0,
+    };
+
+    fetch("http://localhost:3000/grammar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+
+      body: JSON.stringify(newGrammar),
+    })
+      .then((resp) => resp.json())
+      .then((newGrammar) => setArray([...array, newGrammar]));
+
+    setFormData(initialFormValues);
+  };
+
+  return (
+    <div className="form-section">
+      <div className="form container m-auto px-2 mt-10 mb-10" id="contribute">
+        <h2>Add Grammar Resource</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Title"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="image"
+            placeholder="Image URL"
+            value={formData.image}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="link"
+            placeholder="Resource Link"
+            value={formData.link}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default GrammarForm;
