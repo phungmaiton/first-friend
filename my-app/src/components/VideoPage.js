4c4a0ac650c6ee8b715f332d78b6e9e2bfb9d8f3
Katherine West <katherinewest98@yahoo.com>
2023-06-05 03:03:23 -0500

    Added entertainment videos to dbjson

--- my-app/src/components/VideoPage.js
+++ my-app/src/components/VideoPage.js
@@ -1,20 +1,20 @@
-import React from "react";
-import NavBar from "./NavBar";
-import { useState, useEffect } from "react";
-import Banner from "./Banner";
-import ReactPaginate from "react-paginate";
+// import React from "react";
+// import NavBar from "./NavBar";
+// import { useState, useEffect } from "react";
+// import Banner from "./Banner";
+// // import ReactPaginate from "react-paginate";
 
-function VideoPage() {
-  return (
-    <div>
-      <NavBar />
-      <Banner
-        pageTitle="Videos"
-        subTitle="Watch Some Videos"
-        background="https://www.worldatlas.com/r/w768/upload/7f/2c/f0/seoul.jpg"
-      />
-    </div>
-  );
-}
+// function VideoPage() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <Banner
+//         pageTitle="Videos"
+//         subTitle="Watch Some Videos"
+//         background="https://www.worldatlas.com/r/w768/upload/7f/2c/f0/seoul.jpg"
+//       />
+//     </div>
+//   );
+// }
 
-export default VideoPage;
+// export default VideoPage;
