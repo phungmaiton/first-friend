745e4ffd2c40a2c762165642a17c43e90078ce81
Mai <garcia.ton94@gmail.com>
2023-06-06 16:14:23 -0400

    hamburger menu working but links are invisible lol

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -4,22 +4,32 @@ import "../App.css";
 function NavBar() {
   return (
     <nav className="topnav">
-      <div className="container m-auto px-2">
-        <NavLink to="/">
-          <img id="logo" src="./main-logo.png" alt="logo" />
+      {/* <div className="container m-auto px-2"> */}
+      <NavLink to="/">
+        <img id="logo" src="./main-logo.png" alt="logo" />
+      </NavLink>
+      <input id="menu-toggle" type="checkbox" />
+      <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
+      </label>
+      <div className="menu">
+        <NavLink to="/" activeClassName="active">
+          HOME
         </NavLink>
-        {/* <input id="menu-toggle" type="checkbox" />
-        <label class="menu-button-container" for="menu-toggle">
-          <div class="menu-button"></div>
-        </label> */}
-        <div className="menu">
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="/books">BOOKS</NavLink>
-          <NavLink to="/grammar">GRAMMAR</NavLink>
-          <NavLink to="/videos">VIDEOS</NavLink>
-          <NavLink to="/other">OTHER RESOURCES</NavLink>
-        </div>
+        <NavLink to="/books" activeClassName="active">
+          BOOKS
+        </NavLink>
+        <NavLink to="/grammar" activeClassName="active">
+          GRAMMAR
+        </NavLink>
+        <NavLink to="/videos" activeClassName="active">
+          VIDEOS
+        </NavLink>
+        <NavLink to="/other" activeClassName="active">
+          OTHER RESOURCES
+        </NavLink>
       </div>
+      {/* </div> */}
     </nav>
   );
 }
