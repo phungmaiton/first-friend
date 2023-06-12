cdca3fc180578c031e7de7dcd654d7abb9b5d59c
Mai <garcia.ton94@gmail.com>
2023-06-12 14:20:29 -0400

    removed server js

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3030;
-
-app.use(express.static("public"));
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
