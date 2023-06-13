4d3e3af1f67dd4f73f9d9b7105dc600c7f99428f
Mai <garcia.ton94@gmail.com>
2023-06-13 09:19:54 -0400

    hamburger menu close on click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import "../App.css";
+
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="topnav">
-      {/* <div className="container m-auto px-2"> */}
       <NavLink to="/first-friend/">
         <img id="logo" src="./main-logo.png" alt="logo" />
       </NavLink>
-      <input id="menu-toggle" type="checkbox" />
+      <input
+        id="menu-toggle"
+        type="checkbox"
+        checked={menuOpen}
+        onChange={toggleMenu}
+      />
       <label className="menu-button-container" htmlFor="menu-toggle">
         <div className="menu-button"></div>
       </label>
-      <div className="menu">
-        <NavLink to="/first-friend/">HOME</NavLink>
-        <NavLink to="/first-friend/books">BOOKS</NavLink>
-        <NavLink to="/first-friend/grammar">GRAMMAR</NavLink>
-        <NavLink to="/first-friend/videos">VIDEOS</NavLink>
-        <NavLink to="/first-friend/resources">OTHER RESOURCES</NavLink>
+      <div className={`menu ${menuOpen ? "open" : ""}`}>
+        <NavLink to="/first-friend/" onClick={closeMenu}>
+          HOME
+        </NavLink>
+        <NavLink to="/first-friend/books" onClick={closeMenu}>
+          BOOKS
+        </NavLink>
+        <NavLink to="/first-friend/grammar" onClick={closeMenu}>
+          GRAMMAR
+        </NavLink>
+        <NavLink to="/first-friend/videos" onClick={closeMenu}>
+          VIDEOS
+        </NavLink>
+        <NavLink to="/first-friend/resources" onClick={closeMenu}>
+          OTHER RESOURCES
+        </NavLink>
       </div>
-      {/* </div> */}
     </nav>
   );
 }
